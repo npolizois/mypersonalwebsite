71248d2ab1d817e3a4674c51c578c798027367e5
Nikos Polyzois <polyzois.nikos@gmail.com>
2024-09-23 15:07:19 +0300

    test

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero: React.FC = () => {
     >
       <div className="text-center">
         <h1 className="text-5xl font-bold">Nikos Polyzois</h1>
-        <p className="text-xl mt-4">Full Stack Developer</p>
+        <p className="text-xl mt-4">Full Stack Developer blah blah</p>
         <a
           href="#contact"
           className="mt-8 inline-block bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 rounded-lg"
