85e04ddbf0f9c2dcb03afe9aebe53ddad834652e
Nikos Polyzois <polyzois.nikos@gmail.com>
2024-09-25 13:53:01 +0300

    try to fix 404 issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom"; // Use HashRouter
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
