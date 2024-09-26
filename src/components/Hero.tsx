4620c00ed2d817a517ee5ed9e21de08d191b47f9
Nikos Polyzois <polyzois.nikos@gmail.com>
2024-09-26 14:11:11 +0300

    css fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,13 +8,13 @@ interface HeroProps {
 const Hero: React.FC<HeroProps> = ({ setIsOpen }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center container mx-auto px-4 sm:px-2 md:px-4 md:pt-28 md:py-12 lg:pt-12">
-      <h1 className="text-4xl sm:text-6xl font-extrabold text-white drop-shadow-lg text-center">
+      <h1 className="text-4xl lg:text-6xl font-extrabold text-white drop-shadow-lg text-center">
         Senior Full Stack Web Developer
       </h1>
-      <h2 className="text-2xl sm:text-4xl mt-4 text-gray-300 text-center">
+      <h2 className="text-2xl lg:text-4xl mt-4 text-gray-300 text-center">
         Delivering Scalable and High-Performance Web Applications
       </h2>
-      <p className="mt-6 text-lg sm:text-xl text-gray-400 text-center max-w-2xl">
+      <p className="mt-6 text-lg lg:text-xl text-gray-400 text-center max-w-2xl">
         With over 7 years of experience, I specialize in creating efficient and
         innovative solutions, transforming ideas into reality by leveraging the
         latest technologies.
