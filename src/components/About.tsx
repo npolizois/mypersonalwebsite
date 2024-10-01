98c5843e858531a1259a882322a1a1e21cee5f98
Nikos Polyzois <polyzois.nikos@gmail.com>
2024-10-01 14:26:28 +0300

    changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -32,37 +32,40 @@ const About: React.FC<AboutProps> = ({ isNavOpen }) => {
         </div>
       </div>
       <div className="md:ml-8 mt-12 md:mt-0 flex flex-col items-start max-w-full xs:max-w-sm md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl space-y-2">
-        <h2 className="text-3xl md:text-4xl font-bold text-center text-white mb-8 lg:mb-4 xl:mb-8">
+        <h2 className="text-3xl md:text-4xl font-bold text-center text-white mb-8 md:mb-4">
           About Me
         </h2>
         <p className="md:text-xs lg:text-sm xl:text-base text-gray-300">
-          I am Nikos Polyzois, a Full Stack Web Developer with a diverse skill
-          set encompassing web application development, custom ERP systems, and
-          CMS-driven solutions. Bachelor of Science in Computer Engineering from
-          the Technological Educational Institute (TEI) of Athens and a Diploma
-          in Web Design & Development from the Future Business School. With a
-          strong foundation in front-end and back-end technologies, I excel in
-          Typescript, React.js, Node.js, and Angular. I am passionate about
-          creating seamless digital experiences, combining my technical
-          proficiency with a keen eye for design to enhance functionality and
-          user satisfaction.
+          I am a Full Stack Web Developer with over 7 years of experience in web
+          application development, custom ERP system, and CMS-driven solutions.
+          I hold a Bachelor of Science in Computer Engineering from the
+          Technological Educational Institute (TEI) of Athens and a Diploma in
+          Web Design & Development from the Future Business School.
+        </p>
+        <p className="md:text-xs lg:text-sm xl:text-base text-gray-300">
+          With expertise in both front-end and back-end technologies, I excel in
+          TypeScript, React.js, Node.js, and Angular. I am passionate about
+          creating seamless digital experiences that blend technical proficiency
+          with a keen eye for design to enhance functionality and user
+          satisfaction.
         </p>
         <p className="md:text-xs lg:text-sm xl:text-base text-gray-300">
           I am committed to best practices in Test-Driven Development (TDD) and
-          version control, thriving in collaborative environments where I can
-          bring creativity and innovative problem-solving to every project. My
+          version control. I thrive in collaborative environments, bringing
+          creativity and innovative problem-solving to every project. My
           experience includes building e-commerce platforms, developing API
-          interfaces, and providing database administration, all while
-          leveraging advanced AI tools to streamline workflows and enhance
-          productivity.
+          interfaces, and providing database administration while leveraging
+          advanced AI tools to streamline workflows and boost productivity.
         </p>
         <p className="md:hidden lg:block lg:text-sm xl:text-base text-gray-300">
-          In my previous roles as an E-commerce Administrator and IT Specialist,
-          I focused on on-site management, SEO strategies, and IT support for
-          multinational companies. I believe my ability to harmonize technical
-          and creative elements position me as a valuable asset to any
-          development team, ready to tackle complex challenges and drive
-          impactful solutions.
+          I have successfully led Digital Transformation initiatives, helping
+          organizations modernize their processes and adopt new technologies for
+          improved efficiency and competitiveness.
+        </p>
+        <p className="md:hidden lg:block lg:text-sm xl:text-base text-gray-300">
+          I believe my ability to harmonize technical and creative elements
+          makes me a valuable asset to any development team, ready to tackle
+          complex challenges and drive impactful solutions.
         </p>
       </div>
     </motion.div>
